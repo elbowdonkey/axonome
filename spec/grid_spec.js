79508d44de46497a89352ffdca4e1c3312975c5e
Michael Buffington <michael.buffington@gmail.com>
2009-04-24 19:25:13 -0700

    updated to jquery 1.3.2, added nodes, threw in a gnarly demo

--- spec/grid_spec.js
+++ spec/grid_spec.js
@@ -9,5 +9,15 @@ Screw.Unit(function() {
       expect(grid.width()).to(equal,3);
       expect(grid.height()).to(equal,3);
     });
+    
+    it('should have nodes', function() {
+      var nodes = [
+      [0,0],[1,0],[2,0],
+      [0,1],[1,1],[2,1],
+      [0,2],[1,2],[2,2]
+      ]
+      
+      expect(grid.nodes()).to(equal,nodes);
+    });
   });
 });
