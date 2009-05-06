8f2c5526249c3181aacf52f8dbf2e8c8be7e30d9
Michael Buffington <michael.buffington@gmail.com>
2009-05-06 09:51:00 -0700

    Added isometric2 demo which accepts query params to populate grid

--- spec/grid_spec.js
+++ spec/grid_spec.js
@@ -34,5 +34,24 @@ Screw.Unit(function() {
       expect(grid.width()).to(equal,3);
       expect(grid.height()).to(equal,3);
     });
+    
+    it('should return a range of nodes', function() {
+      grid = new Grid({width:6, height:6});
+      var range = {x1: 0, y1: 0, x2: 1, y2: 1};
+      var nodes = grid.nodes(range);
+      
+      expect(nodes.length).to(equal,4);
+      expect(nodes[0].x).to(equal,0);
+      expect(nodes[0].y).to(equal,0);
+      
+      expect(nodes[1].x).to(equal,1);
+      expect(nodes[1].y).to(equal,0);
+      
+      expect(nodes[2].x).to(equal,0);
+      expect(nodes[2].y).to(equal,1);
+       
+      expect(nodes[3].x).to(equal,1);
+      expect(nodes[3].y).to(equal,1);
+    });
   });
 });
